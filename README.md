84fb01d0de0af09acc816d309034c9ee3f47359a
Bas <basroeper@gmail.com>
2021-04-04 10:45:20 +0200

    Nieuwe repository gestart omdat ik github nog niet snap

---
+++ README.md
@@ -0,0 +1,18 @@
+# Gerechten kiezer
+
+Het idee achter dit project is een programma maken waar je kunt invullen voor hoeveel dagen je gerechten wilt kiezenn en hoeveel hiervan vis en vegetarisch moet zijn.
+
+## Status van mijn code
+
+Ik heb nu de volgende dingen werkende
+
+* De database van gerecht
+* De kiezer input
+* De code die vervolgens die juist hoeveelheid gerechten uit de database haalt
+* Code die vergelijkt of ik niet twee keer het zelfde eet
+* Een functie die van de gerechten een mooi zinnetje maakt
+
+## Wat zou ik nog willen
+
+* Een web interface die ik lokaal kan draai
+* Dat ik kan kiezen hoeveel gerechten er veel tijd mogen kosten om voor te bereiden
---
+++ main.js
@@ -0,0 +1,122 @@
+// Alle mogelijke gerechten
+let gerechtenVlees = [   'Risotto Tijm, Rosemarijn (Italiaanse keuken pg 76)',
+                    'Thai curry (boek voor mem)',
+                    'Thom Kha kai (boek voor mem)',
+                    'Beef Basil (bijbel Thai pg 130)',
+                    'Nacho’s (Jamie app)',
+                    'Spanish-style chicken stew (jamie app)',
+                    'Chicken Phal (Jamie app)',]
+let hoofdGerechtenVlees = [
+                    'Gelakte kip (puur genieten pg 46)',
+                    'Beef met Gember (5 ingrediënten 185)',
+                    'Gehaktbal met jus',]
+let bijGrechten = [ 'Spinaziecurry (5 ingrediënten 162)',
+                    'Zoete aardappel met cajun (5 ingrediënten 175)',
+                    'Tomaten salade',
+                    'Gebakken rijst']
+let hoofdGerechtVis = [
+                    'Zalm en geitenkaas saladebolletjes',
+                    'Visragout',
+                    'Zalm gehaktbal (puur genieten pg 86)',
+                    'Vis gemalen komijn, paprika en koriander',
+                    ]
+let gerechtenVis = ['Kabeljauw mosterd sojasaus (puur genieten pg 96)',
+                    'Sesame Seared Tuna & Sushi Bar Spinach']
+let gerechtenVega = [
+                    'Pasta Pesto',
+                    'Couscous',
+                    'Jackfruit stoofschotel',
+                    'Bieten Burger']
+
+
+// Functie kiest willekeurige gerechten en zit die in een array
+
+// Gekozen is de uitkomst van de selectie
+
+let gekozenVlees = []
+let gekozenVis = []
+let gekozenVega = []
+
+// Dit kiest vis gerechten
+const watEtenVis = vis => {
+    do { 
+        gekozenVis = []
+        for (let i = 0; i < vis; i++) {
+            let totaalOfNiet = Math.floor(Math.random()*2)
+            if (totaalOfNiet == 1) {
+                let temp = []
+                const nummer = Math.floor(Math.random()*(hoofdGerechtVis.length-1))
+                temp.push(hoofdGerechtVis[nummer] + ' met')
+                const nummer2 = Math.floor(Math.random()*(bijGrechten.length-1))
+                temp.push(bijGrechten[nummer2])
+                let outPut = temp.join(' ')
+                gekozenVis.push(outPut)
+            } else {
+            const nummer = Math.floor(Math.random()*(gerechtenVis.length-1))
+            gekozenVis.push(hoofdGerechtVis[nummer])
+            }
+        }
+    } while (vergelijk(gekozenVis))
+}
+
+// Dit kiest vleesgerechten
+const watEtenVlees = (hoeveelDagen,vega,vis) => {
+    do {
+        gekozenVlees = []
+        for (let i = 0; i < hoeveelDagen - (vega + vis); i++) {
+            let totaalOfNiet = Math.floor(Math.random()*2)
+            if (totaalOfNiet == 1) {
+                let temp = []
+                const nummer = Math.floor(Math.random()*(hoofdGerechtenVlees.length-1))
+                temp.push(hoofdGerechtenVlees[nummer] + ' met')
+                const nummer2 = Math.floor(Math.random()*(bijGrechten.length-1))
+                temp.push(bijGrechten[nummer2])
+                let outPut = temp.join(' ')
+                gekozenVlees.push(outPut)
+            } else {
+            const nummer = Math.floor(Math.random()*(gerechtenVlees.length-1))
+            gekozenVlees.push(gerechtenVlees[nummer])
+            }
+        }
+    } while (vergelijk(gekozenVlees));
+}
+
+// Dit kiest vega gerechten
+const watEtenVega = vega => {
+    do { 
+        gekozenVega = []
+        for (let i = 0; i < vega; i++) {
+            const nummer = Math.floor(Math.random()*(gerechtenVega.length-1))
+            gekozenVega.push(gerechtenVega[nummer])
+        }
+    } while (vergelijk(gekozenVega))
+}
+
+// Dit stuurt het geheel aan
+const keuze = (hoeveelDagen,vega,vis) => {
+    watEtenVis(vis);
+    watEtenVega(vega);
+    watEtenVlees(hoeveelDagen,vega,vis)
+}
+
+// functie maakt er een mooi zinnetje van
+const wijEten = (hoeveelDagen,vega,vis) => {
+    keuze(hoeveelDagen,vega,vis)
+    const gekozen = gekozenVlees.concat(gekozenVis,gerechtenVega)
+    console.log(`Wij eten: ${gekozen.join(' - ')}`)
+}
+
+const vergelijk = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let y = i + 1; y < arr.length; y++) {
+            if (arr[i] == arr[y]) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+console.log('')
+wijEten(5,2,1)
+console.log('')
