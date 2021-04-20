6a84ed00e3256bf831b06dac926d0000e3e73a09
Bas <basroeper@gmail.com>
2021-04-20 12:50:00 +0200

    ; toegevoegd

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const prompt = require('prompt-sync')({sigint: true});
-const nieuwGerecht = require('./nieuw-gerecht')
+const nieuwGerecht = require('./nieuw-gerecht');
 
 // Alle mogelijke gerechten
-let _gerechtenVlees = ['Nieuw gerecht','Risotto Tijm, Rozemarijn (Italiaanse keuken pg 76)','Thai curry (boek voor mem)','Thom Kha kai (boek voor mem)','Beef Basil (bijbel Thai pg 130)','Nacho’s (Jamie app)','Spanish-style chicken stew (jamie app)','Chicken Phal (Jamie app)',]
-let _hoofdGerechtenVlees = ['Gelakte kip (puur genieten pg 46)','Beef met Gember (5 ingrediënten 185)','Gehaktbal met jus',]
-let _bijGrechten = ['Spinaziecurry (5 ingrediënten 162)','Zoete aardappel met cajun (5 ingrediënten 175)','Tomaten salade','Gebakken rijst']
-let _hoofdGerechtVis = ['Zalm en geitenkaas saladebolletjes','Visragout','Zalm gehaktbal (puur genieten pg 86)','Vis gemalen komijn, paprika en koriander',]
-let _gerechtenVis = ['Kabeljauw mosterd sojasaus (puur genieten pg 96)','Sesame Seared Tuna & Sushi Bar Spinach']
-let _gerechtenVega = ['Pasta Pesto','Couscous','Jackfruit stoofschotel','Tomatensoep hello fresh','Parel Couscous salade hello fresh']
+let _gerechtenVlees = ['Nieuw gerecht','Risotto Tijm, Rozemarijn (Italiaanse keuken pg 76)','Thai curry (boek voor mem)','Thom Kha kai (boek voor mem)','Beef Basil (bijbel Thai pg 130)','Nacho’s (Jamie app)','Spanish-style chicken stew (jamie app)','Chicken Phal (Jamie app)',];
+let _hoofdGerechtenVlees = ['Gelakte kip (puur genieten pg 46)','Beef met Gember (5 ingrediënten 185)','Gehaktbal met jus',];
+let _bijGrechten = ['Spinaziecurry (5 ingrediënten 162)','Zoete aardappel met cajun (5 ingrediënten 175)','Tomaten salade','Gebakken rijst'];
+let _hoofdGerechtVis = ['Zalm en geitenkaas saladebolletjes','Visragout','Zalm gehaktbal (puur genieten pg 86)','Vis gemalen komijn, paprika en koriander',];
+let _gerechtenVis = ['Kabeljauw mosterd sojasaus (puur genieten pg 96)','Sesame Seared Tuna & Sushi Bar Spinach'];
+let _gerechtenVega = ['Pasta Pesto','Couscous','Jackfruit stoofschotel','Tomatensoep hello fresh','Parel Couscous salade hello fresh'];
 
 // Array van gerechten waar ik mee werk
-let gerechtenVlees = []
-let hoofdGerechtenVlees = []
-let bijGrechten = []
-let hoofdGerechtVis = []
-let gerechtenVis = []
-let gerechtenVega = []
+let gerechtenVlees = [];
+let hoofdGerechtenVlees = [];
+let bijGrechten = [];
+let hoofdGerechtVis = [];
+let gerechtenVis = [];
+let gerechtenVega = [];
 
 // Gekozen is de uitkomst van de selectie
-let gekozen = []
+let gekozen = [];
 
 // Hier hussel ik een array
 const hussel = array => {
@@ -34,24 +34,24 @@ const hussel = array => {
 const watEtenVis = vis => {
     for (let i = 0; i < vis; i++) {
         if (hoofdGerechtVis.length == 0 && gerechtenVis.length == 0) {
-            throw 'Niet genoeg vis gerechten'
+            throw 'Niet genoeg vis gerechten';
         }
-        hussel(hoofdGerechtVis)
-        hussel(gerechtenVis)
-        hussel(bijGrechten)
-        let totaalOfNiet = Math.floor(Math.random()*2)
+        hussel(hoofdGerechtVis);
+        hussel(gerechtenVis);
+        hussel(bijGrechten);
+        let totaalOfNiet = Math.floor(Math.random()*2);
         if (totaalOfNiet == 1 && hoofdGerechtVis.length > 0) {
-            let temp = []
-            temp.push(hoofdGerechtVis.pop() + ' met')
-            temp.push(bijGrechten.pop())
-            gekozen.push(temp.join(' '))
+            let temp = [];
+            temp.push(hoofdGerechtVis.pop() + ' met');
+            temp.push(bijGrechten.pop());
+            gekozen.push(temp.join(' '));
         } 
         else {
             if (gerechtenVis.length == 0) {
-                i--
+                i--;
             }
             else {
-                gekozen.push(gerechtenVis.pop())
+                gekozen.push(gerechtenVis.pop());
             }
         }
     }
@@ -89,9 +89,9 @@ const watEtenVega = vega => {
     hussel(gerechtenVega)
     for (let i = 0; i < vega; i++) {
         if (gerechtenVega.length == 0) {
-            throw 'Niet genoeg Vega gerechten'
+            throw 'Niet genoeg Vega gerechten';
         }
-        gekozen.push(gerechtenVega.pop())
+        gekozen.push(gerechtenVega.pop());
         
     }
 }
@@ -100,17 +100,17 @@ const watEtenVega = vega => {
 const keuze = (hoeveelDagen,vega,vis) => {
     watEtenVis(vis);
     watEtenVega(vega);
-    watEtenVlees(hoeveelDagen,vega,vis)
+    watEtenVlees(hoeveelDagen,vega,vis);
 }
 
 // Deze functie maakt er een mooi zinnetje van
 const wijEten = (hoeveelDagen,vega,vis) => {
     keuze(hoeveelDagen,vega,vis)
     if (gekozen.indexOf('Nieuw gerecht') >= 0) {
-        gekozen[gekozen.indexOf('Nieuw gerecht')] = nieuwGerecht() 
+        gekozen[gekozen.indexOf('Nieuw gerecht')] = nieuwGerecht();
     }
     for (let i = 0; i < gekozen.length; i++) {
-        console.log(`${i+1}. ${gekozen[i]}`)
+        console.log(`${i+1}. ${gekozen[i]}`);
     }
 }
 
@@ -121,15 +121,15 @@ const vega = Number(prompt('Hoeveel vegetarisch? '));
 const vis = Number(prompt('Hoevaak vis? '));
 
 do {
-gekozen = []
-gerechtenVlees = _gerechtenVlees.map(x => x)
-hoofdGerechtenVlees = _hoofdGerechtenVlees.map(x => x)
-bijGrechten = _bijGrechten.map(x => x)
-hoofdGerechtVis = _hoofdGerechtVis.map(x => x)
-gerechtenVis = _gerechtenVis.map(x => x)
-gerechtenVega = _gerechtenVega.map(x => x)
+gekozen = [];
+gerechtenVlees = _gerechtenVlees.map(x => x);
+hoofdGerechtenVlees = _hoofdGerechtenVlees.map(x => x);
+bijGrechten = _bijGrechten.map(x => x);
+hoofdGerechtVis = _hoofdGerechtVis.map(x => x);
+gerechtenVis = _gerechtenVis.map(x => x);
+gerechtenVega = _gerechtenVega.map(x => x);
 console.log('');
 wijEten(dagen,vega,vis);
 console.log('');
-happy = prompt('Ben je blij met dit resultaat? Ja of Nee: ').toLowerCase()
+happy = prompt('Ben je blij met dit resultaat? Ja of Nee: ').toLowerCase();
 } while (happy != 'ja');
