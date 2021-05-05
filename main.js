531247e661e98576a04120cce387f32dd8eb84a8
Bas <basroeper@gmail.com>
2021-05-05 09:59:36 +0200

    De gerechten Database is nu los van het programma

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 const prompt = require('prompt-sync')({sigint: true});
 const nieuwGerecht = require('./nieuw-gerecht');
-
-// Alle mogelijke gerechten
-let _gerechtenVlees = ['Nieuw gerecht','Risotto Tijm, Rozemarijn (Italiaanse keuken pg 76)','Thai curry (boek voor mem)','Thom Kha kai (boek voor mem)','Beef Basil (bijbel Thai pg 130)','Nacho’s (Jamie app)','Spanish-style chicken stew (jamie app)','Chicken Phal (Jamie app)',];
-let _hoofdGerechtenVlees = ['Gelakte kip (puur genieten pg 46)','Beef met Gember (5 ingrediënten 185)','Gehaktbal met jus',];
-let _bijGrechten = ['Spinaziecurry (5 ingrediënten 162)','Zoete aardappel met cajun (5 ingrediënten 175)','Tomaten salade','Gebakken rijst'];
-let _hoofdGerechtenVis = ['Zalm en geitenkaas saladebolletjes','Visragout','Zalm gehaktbal (puur genieten pg 86)','Vis gemalen komijn, paprika en koriander',];
-let _gerechtenVis = ['Kabeljauw mosterd sojasaus (puur genieten pg 96)','Sesame Seared Tuna & Sushi Bar Spinach'];
-let _gerechtenVega = ['Fettuccine met pirrige cherrytomatensaus, Simpel pg. 187','Parpapardelle met rozenharissa, Simpel pg. 188','Aardbei Risotto','Pasta Pesto','Couscous','Jackfruit stoofschotel','Tomatensoep hello fresh','Parel Couscous salade hello fresh','Romige risotto en broccoli Jamie 7 ways'];
+const {_bijGrechten,_gerechtenVega,_gerechtenVis,_gerechtenVlees,_hoofdGerechtenVis,_hoofdGerechtenVlees} = require('./gerechten')
 
 // Array van gerechten waar ik mee werk
 let gerechtenVlees = [];
