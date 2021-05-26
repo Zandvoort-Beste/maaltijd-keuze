b655ed6fc158492bf02350a7de13752b8f3b8d20
Bas <basroeper@gmail.com>
2021-05-26 22:11:44 +0200

    vollige verandering van aansturing

--- gerechten.js
+++ gerechten.js
@@ -1,12 +1,13 @@
 class Gerechten {
     constructor (name,personen,bijGerechtReq,difficult,vegaOfVlees,ingredients,source) {
         this._name = name;
-        this._personen = personen
-        this._bijGerechtReq = bijGerechtReq
+        this._personen = personen;
+        this._bijGerechtReq = bijGerechtReq;
         this._difficult = difficult;
         this._type = vegaOfVlees;
         this._ingredients = ingredients;
         this._source = source;
+        this._bijGerecht
     }
     get ingredients () {
         if (this._personen) {
@@ -16,7 +17,20 @@ class Gerechten {
             console.log(`${index+1}: ${this._ingredients[index]}`);
         }
     }
+    get naam () {
+        if (this._bijGerechtReq == false) {
+            return `${this._name} uit ${this._source}`;
+        } else {
+            return `${this._name} uit ${this._source} met ${this._bijGerecht}`
+        }
+    }
+    get bijGerecht () {
+        let random = Math.floor(Math.random() * _bijGerechten.length)
+        this._bijGerecht = _bijGerechten[random]
+    }
 }
+
+// Alle mogelijke hoofd gerechten
 
 const pastaPesto = new Gerechten('Pasta pesto',undefined,false,false,'vlees',['Pasta','Pesto','Rucola','Pijnboom pitten','Parmezaanse kaas','Kip','Cherry tomaten','Knoflook'])
 const beefBasil = new Gerechten('Beef Basil',undefined,false,false,'vlees',['Gehakt','Knoflook','Basilicum','Rijst','Spaanse peper','Basilicum','Soya saus','Vis saus'],'Thai keuken bijbel')
@@ -24,12 +38,15 @@ const jackfruit = new Gerechten('Jackfruit Stoofschotel',4,false,false,'vega',['
 const couscous = new Gerechten('Couscous',undefined,false,false,'vega',['Bouillon', 'Rozijnen', 'Feta', 'Pijnboompitten', 'Knoflook', 'Tomaat', 'Rucola', 'Honing'],'Notitie iOS')
 const nacho = new Gerechten('Nacho',2,false,false,'vlees',['300 gram gehakt',"Nacho's",'1 Rode ui','2 tenen knoflook','2 stengels Bleekselderij','1/2 bunch Koriander','Tijm','Tomaten Pure','2 Jalapeno chillies','2 Limoenen','Gemalen komijn','75g geraspte kaas','Zure room','Olijf olie'],'Jamie Recipes App')
 const fettuccine = new Gerechten('Fettuccine met pittige cherrytomatensaus',4,false,false,'vega',['2 tenen knoflook','1 kg cherrytomaatjes','suiker','1 gedroogde peper','20g basilicum','400g fettucine','35g parmezaan'],'Simpel pg. 187')
-const kabeljauwMosterd = new Gerechten('kabelfauw met mosterd-sojasaus en tomaten',2,false,false,'vis',['2 kabeljauwfilets','4 tomaten','moterd','sojasaus'],'Puur genieten pg. 96')
+const kabeljauwMosterd = new Gerechten('Kabeljauw met mosterd-sojasaus en tomaten',2,false,false,'vis',['2 kabeljauwfilets','4 tomaten','moterd','sojasaus'],'Puur genieten pg. 96')
 const zalmGehakt = new Gerechten('Gehakt van zalm met kruiden en wasabimoyanaise',2,false,false,'vis',['2 zalm filets','2 lente-uitjes','1/2 verse rode chilipeper','2 knoflookteentjes','1 bofje koriander','Sap van 1 limoen','Sesamzaad','Geroosterde sesamolie'],'Puur genieten pg. 86')
 const gelakteKip = new Gerechten('Gelakte kip met boontjes',2,true,false,'vlees',['2 kipfilets','Balsamicoazijn','Sojasaus'],'Puur genieten pg. 46')
 
-// Alle mogelijke hoofd gerechten
 let _gerechten = [pastaPesto,beefBasil,jackfruit,couscous,nacho,fettuccine,kabeljauwMosterd,zalmGehakt,gelakteKip]
+
+// Alle bijgerechten
+
+let _bijGerechten = ['Zoete aardappel Cajun','Test 2', 'Test 3']
 
 module.exports = {
     _gerechten
--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const prompt = require('prompt-sync')({sigint: true});
 const nieuwGerecht = require('./nieuw-gerecht');
-const {_gerechten} = require('./gerechten')
+const {_gerechten} = require('./gerechten');
+let vleesGerechten = []
+let visGerechten = []
+let vegaGerechten = []
 
 const pyPop = (array, index) => {
     let result = array[index]
@@ -8,76 +11,75 @@ const pyPop = (array, index) => {
     return result
 }
 
-const randomLaag = arrayLength => {
-    return Math.floor(Math.random() * arrayLength);
+const randomLaag = array => {
+    return Math.floor(Math.random() * array.length);
+}
+
+const sorteren = (gerechtenArray) => {
+    vleesGerechten = []
+    visGerechten = []
+    vegaGerechten = []
+    for (let i = 0; i < gerechtenArray.length; i++) {
+        switch (gerechtenArray[i]._type) {
+            case 'vlees':
+                vleesGerechten.push(gerechtenArray[i]);
+                break;
+            case 'vis':
+                visGerechten.push(gerechtenArray[i]);
+                break;
+            case 'vega':
+                vegaGerechten.push(gerechtenArray[i]);
+                break;
+            default:
+                throw 'Sorten is mislukt'
+                break;
+        }
+        
+    }
 }
 
 // Gekozen is de uitkomst van de selectie
 let gekozen = [];
 
-// Hier kies ik vlees
-const watEtenVlees = (vlees) => {
-    let hoeveelVlees = 0;
-    while (hoeveelVlees < vlees) {
-        let random = randomLaag(_gerechten.length)
-        if (_gerechten[random]._type == 'vlees') {
-            if (hoeveelVlees < vlees) {
-                gekozen.push(pyPop(_gerechten, random));
-                hoeveelVlees++;
-            }
-        }
-    }
-}
-
-// Hier kies ik vis
-const watEtenVis = (vis) => {
-    let hoeveelVis = 0;
-    while (hoeveelVis < vis) {
-        let random = randomLaag(_gerechten.length)
-        if (_gerechten[random]._type == 'vis') {
-            if (hoeveelVis < vis) {
-                gekozen.push(pyPop(_gerechten, random));
-                hoeveelVis++;
-            }
-        }
-    }
-}
-
-// Hier kies ik vega
-const watEtenVega = (vega) => {
-    let hoeveelVega = 0;
-    while (hoeveelVega < vega) {
-        let random = randomLaag(_gerechten.length)
-        if (_gerechten[random]._type == 'vega') {
-            if (hoeveelVega < vega) {
-                gekozen.push(pyPop(_gerechten, random));
-                hoeveelVega++;
-            }
-        }
+// Hier kies ik de gerechten
+const watEten = (type,hoeveel) => {
+    for (let i = 0; i < hoeveel; i++) {
+        let random = randomLaag(type);
+        gekozen.push(pyPop(type, random));
     }
 }
 
 const aansturen = (vis,vlees,vega) => {
-    watEtenVega(vega);
-    watEtenVis(vis);
-    watEtenVlees(vlees);
+    sorteren(_gerechten)
+    gekozen = []
+    watEten(vegaGerechten,vega);
+    watEten(visGerechten,vis);
+    watEten(vleesGerechten,vlees);
 }
 
 const boodschappen = array  => {
-    console.log('\n Deze boodschappen zijn nodig \n');
+    console.log('\n Deze boodschappen zijn nodig: \n');
     for (let i = 0; i < array.length; i++) {
         console.log(array[i]._name);
         array[i].ingredients;
         console.log('');
     }
 }
+
 console.clear()
 //let hoevaak = prompt('Hoe vaak vega? ');
-aansturen(1,2,2)
+let happy = ''
+do {
+    aansturen(1,1,1)
 for (let i = 0; i < gekozen.length; i++) {
-    console.log(`${i+1}: ${gekozen[i]._name} uit ${gekozen[i]._source}`);
+    if (gekozen[i]._bijGerechtReq) {
+        gekozen[i].bijGerecht;
+    }
+    console.log(`${i+1}: ${gekozen[i].naam}`);
 }
-let happy = prompt('Blij met die resultaat? ').toLowerCase();
+happy = prompt('Blij met die resultaat? ').toLowerCase();
 if (happy == "ja") {
     boodschappen(gekozen);
 }
+} while (happy != 'ja');
+
