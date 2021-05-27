4ec96d7d4b7e4aef8b93b3d756624fd051e94eb0
Bas <basroeper@gmail.com>
2021-05-27 16:10:21 +0200

    kleine aanpassingen aan ingredienten presentatie

--- gerechten.js
+++ gerechten.js
@@ -1,14 +1,15 @@
 class Gerechten {
-    constructor (name,personen,bijGerechtReq,difficult,vegaOfVlees,ingredients,source) {
+    constructor (name,personen,bijGerechtReq,difficult,type,ingredients,source) {
         this._name = name;
         this._personen = personen;
         this._bijGerechtReq = bijGerechtReq;
         this._difficult = difficult;
-        this._type = vegaOfVlees;
+        this._type = type;
         this._ingredients = ingredients;
         this._source = source;
     }
     get ingredients () {
+        console.log(`${this._name}`)
         if (this._personen) {
             console.log(`${this._personen} porties \n`)
         }
@@ -38,7 +39,7 @@ const bijGerechten = require('./main')
 // Alle mogelijke hoofd gerechten
 
 const pastaPesto = new Gerechten('Pasta pesto',undefined,false,false,'vlees',['Pasta','Pesto','Rucola','Pijnboom pitten','Parmezaanse kaas','Kip','Cherry tomaten','Knoflook'])
-const beefBasil = new Gerechten('Beef Basil',4,false,false,'vlees',['300g gehakt','2 el vissaus','2 el sojasaus','1tl suiker','Basilicum','Verse rode pepers'],'Thai keuken bijbel')
+const beefBasil = new Gerechten('Beef Basil',2,false,false,'vlees',['300g gehakt','2 el vissaus','2 el sojasaus','1tl suiker','Basilicum','Verse rode pepers'],'Thai keuken bijbel')
 const jackFruit = new Gerechten('Jackfruit Stoofschotel',4,false,false,'vega',['1 blik jackfruit 500 ml','2 teentjes knoflook fijngehakt','1 ui in ringen','1 winterwortel in blokjes','2 stengels bleekselderij in blokjes','1 laurierblad','2 kruidnagels','1 tl korianderpoeder','1 tl gemberpoeder','0,5 tl nootmuskaatpoeder','1 tl mosterd','40 gram ontbijtkoek','1 el bloem','70 gram tomatenpuree','350 ml groentebouillon','Zonnebloemolie','Peper en zout'],'https://miljuschka.nl/jackfruitstoofschotel/')
 const couscous = new Gerechten('Couscous',undefined,false,false,'vega',['Bouillon', 'Rozijnen', 'Feta', 'Pijnboompitten', 'Knoflook', 'Tomaat', 'Rucola', 'Honing'],'Notitie iOS')
 const nacho = new Gerechten('Nacho',2,false,false,'vlees',['300 gram gehakt',"Nacho's",'1 Rode ui','2 tenen knoflook','2 stengels Bleekselderij','1/2 bunch Koriander','Tijm','Tomaten Pure','2 Jalapeno chillies','2 Limoenen','Gemalen komijn','75g geraspte kaas','Zure room','Olijf olie'],'Jamie Recipes App')
@@ -49,7 +50,7 @@ const gelakteKip = new Gerechten('Gelakte kip met boontjes',2,true,false,'vlees'
 const zoeteAardappel = new Gerechten('Zoete Aardappel met Cajun',4,false,false,'bijGerecht',['4 zoete aardappels van 250g elk','1 bol knoflook','1 tl cajunkruiden','200ml Griekse yoghurt','4 lente-uitjes'],'5 ingredienten pg. 174')
 const spinazieCurry = new Gerechten('Speedy Spinaziecurry',2,false,false,'bijGerecht',['20g ongezouten cashewnoten','1 ui','2tl rogan josh-currypasta','100g feta','200g jonge spinazie'],'5 ingredienten pg. 162')
 
-let _gerechten = [spinazieCurry,pastaPesto,beefBasil,jackFruit,couscous,nacho,fettuccine,kabeljauwMosterd,zalmGehakt,gelakteKip,zoeteAardappel]
+let _gerechten = [pastaPesto,beefBasil,jackFruit,couscous,nacho,fettuccine,kabeljauwMosterd,zalmGehakt,gelakteKip,zoeteAardappel,spinazieCurry]
 
 // Alle bijgerechten
 
--- main.js
+++ main.js
@@ -70,27 +70,24 @@ const aansturen = (vis,vlees,vega) => {
 const boodschappen = array  => {
     console.log('\n Deze boodschappen zijn nodig: \n');
     for (let i = 0; i < array.length; i++) {
-        console.log(array[i]._name);
         array[i].ingredients;
         console.log('');
     }
 }
 
 console.clear()
-//let hoevaak = prompt('Hoe vaak vega? ');
 let happy = '';
 do {
-    aansturen(0,4,0);
-    console.log(gekozen)
-for (let i = 0; i < gekozen.length; i++) {
-    if (gekozen[i]._bijGerechtReq) {
+    aansturen(2,2,2);
+    for (let i = 0; i < gekozen.length; i++) {
+        if (gekozen[i]._bijGerechtReq) {
         bijGerechtToevoegen(gekozen[i]);
+        }
+        console.log(`${i+1}: ${gekozen[i].naam}`);
     }
-    console.log(`${i+1}: ${gekozen[i].naam}`);
-}
-happy = prompt('Blij met die resultaat? ').toLowerCase();
-if (happy == "ja") {
-    boodschappen(gekozen);
-}
+    happy = prompt('Blij met die resultaat? ').toLowerCase();
+    if (happy == "ja") {
+        boodschappen(gekozen);
+    }
 } while (happy != 'ja');
 
