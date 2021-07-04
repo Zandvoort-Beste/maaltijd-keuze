016e2152172366b2616597656728d3b9f9040ce8
Bas <basroeper@gmail.com>
2021-07-04 18:41:34 +0200

    avocado hollandaise toegevoegd

--- gerechten.js
+++ gerechten.js
@@ -73,8 +73,8 @@ const tomatenSoep = new Gerechten('Tomaat-paprikasoep met parelcouscous',2,false
 const parelcouscousSalade = new Gerechten('Linzen-parelcouscoussalade',2,false,false,'vega',['80g parelcouscous','1/2 pak linzen','1 rode ui','1 limoen','10g walnootstukjes','1 appel','2 gekookte rode bieten','5g verse dille','Yoghurt','Tahine','40g sla','75g verse geitenkaas','Groentebouillon','1 el honing','2 el zwarte balsamicoazijn','2 el extra vierge olijfolie'],'Hello fresh')
 const gehaktBal = new Gerechten('Gehakt Bal',2,true,false,'vlees',['300g gehakt','1/2 ui','1 ei','Gehakt kruiden','Cracker','Wat we nog meer willen'])
 const chickenPhal = new Gerechten('Fiendish Chicken Phal',4,false,false,'vlees',['4 kippedijen','3 el rogan josh curry paste','2 gedroogde pepers','1 grote rode ui','1 paprika','1 kaneel stok','1/3 blok kip bouillon','1/2 el azijn','1/2 tl suiker','75g yoghurt','10g boter'],'Jamie Recipes App')
-
-let _gerechten = [pastaPesto,beefBasil,jackFruit,couscous,nacho,fettuccine,kabeljauwMosterd,zalmGehakt,gelakteKip,zoeteAardappel,spinazieCurry,kabeljauwKomijnPaprika,ZalmGeitenkaas,visragout,aardbeiRisotto,rodeCurry,tomKhaKai,rodeCurryVega,tomKhaKaiVega,gebakkenRijst,tomatenSalade,gemberBeef,risottoWorst,broccoliRisotto,tomatenSoep,parelcouscousSalade,gehaktBal,chickenPhal]
+const avocadoHollandaise = new Gerechten('Avocado Hollandaise',2,false,false,'vis',['350g asperges','2 witvis filet','2 lente uien','1 limoen','10g dragon','1 avocado','1 groot ei'],'7 ways pg 48')
+let _gerechten = [avocadoHollandaise,pastaPesto,beefBasil,jackFruit,couscous,nacho,fettuccine,kabeljauwMosterd,zalmGehakt,gelakteKip,zoeteAardappel,spinazieCurry,kabeljauwKomijnPaprika,ZalmGeitenkaas,visragout,aardbeiRisotto,rodeCurry,tomKhaKai,rodeCurryVega,tomKhaKaiVega,gebakkenRijst,tomatenSalade,gemberBeef,risottoWorst,broccoliRisotto,tomatenSoep,parelcouscousSalade,gehaktBal,chickenPhal]
 
 module.exports = {
     _gerechten
